6c8d92a3926c57f53cc6b2551bab4e292e5c3a51
Czertokrj <161880160+Czertokrj@users.noreply.github.com>
2024-03-02 17:03:36 -0300

    Update SSXdevice.ino

--- SSXDevice/examples/SSXdevice.ino
+++ SSXDevice/examples/SSXdevice.ino
@@ -11,14 +11,14 @@ void setup() {
 }
 
 void loop() {
-    const char* hashCentral = "DSV";
-    const char* equipamento = "280224";
-    int codEvento = 1;
+    const char* hashCentral = "DSV";     // hashcode da sua empresa;
+    const char* equipamento = "280224"; // crie um código único para seu dispositivo;
+    int codEvento = 1; // vide lista de eventos
     double latitude = -22.5587487;
     double longitude = -42.5587487;
-    int entradas = 1;
-    int saidas = 0;
-    TelemetriaData telemetria[] = {{17, 60}, {9, 30}, {15, 1500}};
+    int entradas = 1;   // decimal que representa a máscara em binário. ex. 000001;
+    int saidas = 0; // decimal que representa a máscara em binário. ex. 000001;
+    TelemetriaData telemetria[] = {{17, 60}, {9, 30}, {15, 1500}}; // vide lista de telemetrias;
     int idMotorista = 1212;
 
     if(myDevice.sendDeviceData(hashCentral, equipamento, codEvento, latitude, longitude, entradas, saidas, telemetria, 3, idMotorista)) {
